2cdb5ded743032827f8b2016a238cef3d0969315
Darlan0307 <darlanchagas2020@gmail.com>
2024-01-01 21:32:53 -0300

    :lipstick: descrição na pagina home

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -8,18 +8,26 @@ export default function Home(){
 
   return(
     <div className="home_page">
+      <div className='card'>
       <div className='option'
       onClick={()=>navigate("/userform")}
       >
         <FaUserPlus/>
         <span>Adicionar Usuário</span>
       </div>
+      <p>Cadastrar novos usuários usando uma <strong>api</strong> que se conecta a um banco de dados online(mongodb).</p>
+      </div>
+      
+      <div className='card'>
       <div className='option'
       onClick={()=>navigate("/gerenciamento")}
       >
         <FaUserCog/>
         <span>Gerenciar Usuários</span>
       </div>
+      <p>Vizualisar,editar e remover usuários do banco de dados.</p>
+      </div>
+      
     </div>
   )
 }
